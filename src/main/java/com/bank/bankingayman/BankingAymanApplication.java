51c0a733877fecba1c407b69b5db4bcf7ec57f3d
aymane-farajallah <aymanhajiqa@gmail.com>
2026-01-26 11:47:43 +0100

    structure of the project initialized

---
+++ src/main/java/com/bank/bankingayman/BankingAymanApplication.java
@@ -0,0 +1,13 @@
+package com.bank.bankingayman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAymanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingAymanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bank/bankingayman/BankingAymanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.bankingayman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAymanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
